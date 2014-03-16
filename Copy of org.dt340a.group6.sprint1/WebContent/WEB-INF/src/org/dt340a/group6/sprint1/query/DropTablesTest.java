8720edcb93bddd7a01c7c1df15b33785e85bb2a2
Jessie Ryan <jesiryan@gmail.com>
2014-03-16 13:46:04 +0000

    Added the Mockito jars

---
+++ Copy of org.dt340a.group6.sprint1/WebContent/WEB-INF/src/org/dt340a/group6/sprint1/query/DropTablesTest.java
@@ -0,0 +1,19 @@
+package org.dt340a.group6.sprint1.query;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DropTablesTest {
+
+	@Test
+	public final void testIfTablesExist() {
+//		Mockito mockDropTables = DropTables.ifTablesExist();
+	}
+
+	@Test
+	public final void testMain() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
