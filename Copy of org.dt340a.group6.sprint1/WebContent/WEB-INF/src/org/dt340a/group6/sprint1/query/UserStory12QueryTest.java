c22812c419328a701534443505d886e9cccfcbbe
Jessie Ryan <jesiryan@gmail.com>
2014-03-16 23:36:16 +0000

    rewrote two tests

--- Copy of org.dt340a.group6.sprint1/WebContent/WEB-INF/src/org/dt340a/group6/sprint1/query/UserStory12QueryTest.java
+++ Copy of org.dt340a.group6.sprint1/WebContent/WEB-INF/src/org/dt340a/group6/sprint1/query/UserStory12QueryTest.java
@@ -2,7 +2,10 @@ package org.dt340a.group6.sprint1.query;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.xml.rpc.Call;
 
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -19,19 +22,14 @@ public class UserStory12QueryTest {
 	
 	@Test
 	public final void testIncorrectReturnFindAll() {
-		UserStory12Query mockedQueryClass = Mockito.mock(UserStory12Query.class);
-		List<Cause> mockedCauseList = Mockito.mock(List.class);
-		List<CallFailure> mockedCallFailureList = Mockito.mock(List.class);
-		Mockito.when(mockedQueryClass.findAll()).thenReturn(mockedCallFailureList);
-		assertNotSame(mockedQueryClass.findAll(), mockedCauseList);
+		UserStory12Query usq = new UserStory12Query();		
+		assertNotSame(usq.findAll().get(0).getClass(), Cause.class );
 	}
 
 	@Test
 	public final void testCorrectFindAll() {
-		UserStory12Query mockedQueryClass = Mockito.mock(UserStory12Query.class);
-		List<CallFailure> mockedCallFailureList = Mockito.mock(List.class);
-		Mockito.when(mockedQueryClass.findAll()).thenReturn(mockedCallFailureList);
-		assertSame(mockedQueryClass.findAll(), mockedCallFailureList);
+		UserStory12Query usq = new UserStory12Query();		
+		assertSame(usq.findAll().get(0).getClass(), CallFailure.class );
 	}
 
 	@Test
