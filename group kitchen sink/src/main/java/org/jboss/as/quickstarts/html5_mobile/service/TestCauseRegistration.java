2d9ae5617f5214676147a8ed887dfbe6e7e1e6b0
jesiryan <jesiryan@gmail.com>
2014-04-01 16:32:34 +0100

    updated Callfailure entity class to have new constructor added CauseRegistration class & TestCauseRegistration

---
+++ group kitchen sink/src/main/java/org/jboss/as/quickstarts/html5_mobile/service/TestCauseRegistration.java
@@ -0,0 +1,27 @@
+package org.jboss.as.quickstarts.html5_mobile.service;
+
+import static org.junit.Assert.*;
+
+import java.awt.List;
+
+import org.junit.Test;
+
+import com.conygre.training.entities.Callfailure;
+import com.conygre.training.entities.Cause;
+import com.conygre.training.entities.CausePK;
+
+public class TestCauseRegistration {
+
+	@Test
+	public void testPersistCause() {
+//		Cause cause = new Cause();
+//		List callfailures = new List();
+//		CausePK causePK = new CausePK();
+//		causePK.setCauseCode(001);
+//		causePK.setEventId(111);
+//		cause.setCallfailures((java.util.List<Callfailure>) callfailures);
+//		cause.setDescription("description");
+//		cause.setId(causePK);
+	}
+
+}
